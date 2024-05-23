7af13968d71e92dfd84668c944f527bceb1d5f78
balakrishnan4591 <balakrishnan4591@gmail.com>
2024-05-23 23:43:01 +0530

    Day 17 Assignment

---
+++ script.js
@@ -0,0 +1,112 @@
+async function getCountryDetails() {
+  try {
+    var response = await fetch("https://restcountries.com/v3.1/all");
+    let countries = await response.json();
+
+    var count = countries.length;
+    //var count = 3;
+
+    var containerDiv = document.createElement("div"),
+      heading = document.createElement("h1"),
+      rowDiv = document.createElement("div");
+
+    containerDiv.setAttribute("class", "container");
+    heading.setAttribute("id", "title");
+    heading.setAttribute("class", "text-center");
+    heading.innerHTML = "REST Countries";
+    rowDiv.setAttribute("class", "row");
+
+    document.body.append(containerDiv);
+    containerDiv.append(heading, rowDiv);
+
+    for (i = 0; i < count; i++) {
+      var collg4Div = document.createElement("div"),
+        // colsm12Div = document.createElement("div"),
+        cardDiv = document.createElement("div"),
+        // cardHeaderDiv = document.createElement("div"),
+        cardBodyDiv = document.createElement("div"),
+        cardBodyHearder = document.createElement("h3"),
+        cardImg = document.createElement("img"),
+        cardContentCapital = document.createElement("p"),
+        cardContentRegion = document.createElement("p"),
+        cardContentCountryCode = document.createElement("p"),
+        // cardContentLatLng = document.createElement("p"),
+        cardWeatherButton = document.createElement("button");
+
+      collg4Div.setAttribute("class", "col-lg-4 col-sm-12");
+      //colsm12Div.setAttribute("class", "col-sm-12");
+      cardDiv.setAttribute("class", "card");
+      cardDiv.setAttribute("id", "gradient-background");
+
+      // cardHeaderDiv.setAttribute("class", "card-header");
+      cardBodyHearder.setAttribute("class", "card-header");
+      cardBodyHearder.style.textAlign = "center";
+      cardBodyDiv.setAttribute("class", "card-body");
+      cardBodyDiv.style.textAlign = "center";
+      //cardBodyDiv.setAttribute("id", "card-body");
+      cardImg.setAttribute("class", "card-img-top");
+      // cardContentCapital.setAttribute("class", "card-title content");
+      cardContentCapital.setAttribute("class", "card-title content");
+      cardContentRegion.setAttribute("class", "card-title content");
+      cardContentCountryCode.setAttribute("class", "card-title content");
+      //cardContentLatLng.setAttribute("class", "card-title content");
+      // cardContentLatLng.setAttribute("id", "latlng");
+      cardWeatherButton.setAttribute("class", "btn btn-primary");
+      cardWeatherButton.setAttribute("type", "button");
+
+      cardBodyHearder.innerHTML = countries[i].name.common;
+      cardImg.setAttribute("src", countries[i].flags.png);
+      //cardImg.style.width = "150px";
+      cardContentCapital.innerHTML = "Capital: " + countries[i].capital;
+      cardContentRegion.innerHTML = "Region: " + countries[i].region;
+      cardContentCountryCode.innerHTML = "Country Code: " + countries[i].cca3;
+
+      cardWeatherButton.textContent = "Click for Weather";
+
+      //using closure to avoid index issues
+      (function (index) {
+        cardWeatherButton.addEventListener("click", () => {
+          var lat = countries[index].latlng[0];
+          var lng = countries[index].latlng[1];
+          var country = countries[index].name.common;
+          getWeather(country, lat, lng);
+        });
+      })(i);
+
+      rowDiv.append(collg4Div);
+      //collg4Div.append(colsm12Div);
+      collg4Div.append(cardDiv);
+      // cardDiv.append(cardHeaderDiv);
+      cardDiv.append(cardBodyHearder, cardBodyDiv);
+      //cardHeaderDiv.append();
+      cardBodyDiv.append(
+        cardImg,
+        cardContentCapital,
+        cardContentRegion,
+        cardContentCountryCode,
+        cardWeatherButton
+      );
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function getWeather(country, latitude, longitude) {
+  let weatherdata = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=0e7160dfc0fa55f2d6cd7162440b7f0d`
+  );
+
+  let response = await weatherdata.json();
+
+  alert(
+    country +
+      "'s" +
+      " Current Temperature is " +
+      response.main.temp +
+      " and Humidity is " +
+      response.main.humidity
+  );
+}
+
+getCountryDetails();
